477053ce9ddcb4cc28ed8345b18a98a5b172c16e
Ognjen Sredic <nsredic@sbb.rs>
2022-01-21 18:53:04 +0100

    Add text file storage

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
+        /// <summary>
+        /// Saves a new prize to a text file
+        /// </summary>
+        /// <param name="model">The prize information</param>
+        /// <returns>The prize information plus the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //Load the text file
+            //Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            //Find the max ID
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            //Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+
+            //Convert the prizes to List<string>
+            //Save the List<string> to the new file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
