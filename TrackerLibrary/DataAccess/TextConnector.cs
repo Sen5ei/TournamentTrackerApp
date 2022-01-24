e274870d83bd60fca5f864c91cd738ee81451f73
Ognjen Sredic <nsredic@sbb.rs>
2022-01-24 22:46:52 +0100

    Create Team Form - Create Team Wire Up

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -12,6 +12,7 @@ namespace TrackerLibrary.DataAccess
     {
         private const string PrizesFile = "PrizeModels.csv";
         private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv";
 
         /// <summary>
         /// Saves a new person to a text file
@@ -51,8 +52,7 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information plus the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            // Load the text file
-            // Convert the text to List<PrizeModel>
+            // Load the text file and convert the text to List<PrizeModel>
             List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
 
             // Find the max ID
@@ -71,6 +71,32 @@ namespace TrackerLibrary.DataAccess
             // Convert the prizes to List<string>
             // Save the List<string> to the text file
             prizes.SaveToPrizeFile(PrizesFile);
+
+            return model;
+        }
+
+        /// <summary>
+        /// Saves a new team to the text file
+        /// </summary>
+        /// <param name="model">The team information</param>
+        /// <returns>The team information plus the unique identifier</returns>
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            // Find the max ID
+            int currentId = 1;
+
+            if (teams.Count > 0)
+            {
+                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            teams.Add(model);
+
+            teams.SaveToTeamFile(TeamFile);
 
             return model;
         }
